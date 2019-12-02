1c67ae9f32d4d6e02c60e253089607575e2d00aa
David Tauraso <david318@me.com>
2019-12-02 12:18:32 -0800

    Initial setup

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+
+
+    constructor() {
+        super()
+        this.state = {
+            newTodo: ""
+        };
+    }
+
+    handleFormChanges = changeEvent => {
+
+
+    }
+    handleFormSubmit = submitEvent => {
+
+    }
+
+    render() {
+        
+        return ()
+    }
+}
